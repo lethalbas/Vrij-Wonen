7f03b398af4dd522a1d2e0b93568eb28d155b07a
lethalbas <lethalbas@gmail.com>
2023-01-28 15:52:51 +0100

    added forbidden to router

--- app/util/router_util.php
+++ app/util/router_util.php
@@ -46,6 +46,9 @@ class router_util {
             case '/api' :
                 require_once  __DIR__ . '/../api/api.php';
                 break;
+            case '/forbidden' :
+                require_once  __DIR__ . '/../view/forbidden.php';
+                break;
             default:
                 http_response_code(404);
                 require_once  __DIR__ . '/../view/404.php';
