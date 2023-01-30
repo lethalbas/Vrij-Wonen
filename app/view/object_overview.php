a013596c27f717b40d9e33ab9d73a038f75e21f7
lethalbas <lethalbas@gmail.com>
2023-01-30 14:47:25 +0100

    fixed filter bug (returned emty data when no filters selected)

--- app/view/object_overview.php
+++ app/view/object_overview.php
@@ -3,7 +3,7 @@ session_start();
 require_once __DIR__ . "/../controller/objects_controller.php"; 
 require_once __DIR__ . "/../controller/properties_controller.php"; 
 require_once __DIR__ . "/../controller/cities_controller.php"; 
-$searched = isset($_POST["properties"]) || isset($_POST["citie"]);
+$searched = (isset($_POST["properties"]) || isset($_POST["citie"]) && $_POST["citie"] != "");
 if(isset($_POST["properties"])){
     $prop = implode(",", $_POST["properties"]);
 }
